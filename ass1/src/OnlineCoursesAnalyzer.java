81f4c5669522476045f5c276e550528c27dd4ea2
Vancy0 <95403868+Vancy0@users.noreply.github.com>
2023-03-08 15:14:11 +0800

    Create basic skeleton of this assignment including the class and its methods

---
+++ ass1/src/OnlineCoursesAnalyzer.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.Map;
+
+public class OnlineCoursesAnalyzer {
+
+
+    public OnlineCoursesAnalyzer(String datasetPath) {
+
+    }
+
+    public Map<String, Integer> getPtcpCountByInst() {
+        return null;
+    }
+
+    public Map<String, Integer> getPtcpCountByInstAndSubject(){
+        return null;
+    }
+
+    public Map<String, List<List<String>>> getCourseListOfInstructor(){
+        return null;
+    }
+
+    public List<String> getCourses(int topK, String by){
+        return null;
+    }
+
+    public List<String> searchCourses(String courseSubject, double
+            percentAudited, double totalCourseHours){
+        return null;
+    }
+
+    public List<String> recommendCourses(int age, int gender, int
+            isBachelorOrHigher){
+        return null;
+    }
+}
