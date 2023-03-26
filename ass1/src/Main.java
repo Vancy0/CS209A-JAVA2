0f5c2ff30a480c3b2805ba7b2136fbe342fbfe0f
Vancy0 <95403868+Vancy0@users.noreply.github.com>
2023-03-26 23:14:38 +0800

    Reduce checkStyle error again and again

--- ass1/src/Main.java
+++ ass1/src/Main.java
@@ -1,4 +1,6 @@
+@SuppressWarnings("checkstyle:MissingJavadocType")
 public class Main {
+    @SuppressWarnings("checkstyle:Indentation")
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
